f30fe11df38572eeee2a6bbe5066b4d689dd879e
abdelneuhaus <abdelghani.neuhaus@etu.u-bordeaux.fr>
2020-03-17 19:04:19 +0100

    Ajout des factures et retrait

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ void afficheMenu(){
     cout << "0: Quitter" << "\n";
     cout << "1: Saisie d'un client et de sa facture" << "\n";
     cout << "2: Afficher les informations d'un client" << "\n";
+    cout << "3: Saisie du départ d'un client" << "\n";
+    cout << "4: Afficher les places disponibles selon le type de bateau" << "\n";
 }
 
 int main(){
@@ -19,16 +21,23 @@ int main(){
     while(true){
         afficheMenu();
         int choix;
+        cout << ">> ";
         cin >> choix;
         if(choix == 0){
             return(0);
         }
         else if(choix == 1){
             Clients = GestionLaRochelle.enregistreClient(Clients);
-            Clients = GestionLaRochelle.saisieFacture(Clients);
+            // Clients = GestionLaRochelle.saisieFacture(Clients);
         }
         else if(choix == 2){
             GestionLaRochelle.afficheInfos(Clients);
+        }
+        else if(choix == 3){
+            Clients = GestionLaRochelle.retirerClient(Clients);
+        }
+        else if(choix == 4){
+            GestionLaRochelle.afficherPlaces();
         }
     }
     return 0;
